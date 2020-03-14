75cd68300d1f81d1545d65afcc6900a64ad259d2
Daniel Sajdyk <kontakt@sajdyk.pl>
2020-03-14 22:40:00 +0100

    Poprawiono walidacje parametru -sciezka na 'ksiazkowa', tzn. poprzez opcje 'Param' zamiast 'if'. Do poprawy / zastanowienia sie co zrobic z regexem, bo w tym momencie obcina on sciezki plikow konczacych sie na mp3, mp4 itd.

--- Get-ControlSumFromOpticalDrive.ps1
+++ Get-ControlSumFromOpticalDrive.ps1
@@ -1,25 +1,36 @@
+[CmdletBinding()]
 Param(
     [Parameter(Mandatory=$false, ValueFromPipeline=$true)]
+    [ValidateScript({
+        if (Test-Path -Path $_){
+            $true
+        }
+        else {
+            throw "Folder $_ nie istnieje"
+        }
+    })]
     $sciezka = (Get-CimInstance Win32_LogicalDisk | Where-Object {$_.DriveType -eq 5} | Select-Object DeviceID -ExpandProperty DeviceID)
 )
 
 
-$global:i=0
+$script:i=0
+
+# nie zmieniać obslugi bledow na try catch poniewaz wymaga on wygenerowania bledu przerywajacego, ktory nie wystepuje w przypadku Test-Path
 if (Test-Path -Path "$sciezka"){
     get-childitem `
     -Path $sciezka -Recurse -File -Exclude "player.zip" | `
     get-filehash -Algorithm MD5 | `
     Format-Table -Autosize -property `
-    @{Name="Lp";Expression={$global:i.Tostring() + ".";$global:i++}}, `
+    @{Name="Lp";Expression={$script:i.Tostring() + ".";$script:i++}}, `
     @{Name="Algorytm"; Expression = {$_.algorithm}}, hash, `
     @{Name="Plik"; Expression = {$_.Path -replace '(^.*)\\(.*.mp\d$)','$2'}} # | Out-Printer ; 
 }
 else {
-    write-error "W NAPEDZIE BRAKUJE PLYTY, BADZ WSKAZANY FOLDER - $sciezka - NIE JEST PRAWIDLOWY."
+    Write-Error "W NAPEDZIE BRAKUJE PLYTY, BADZ WSKAZANY FOLDER - $sciezka - NIE JEST PRAWIDLOWY."
 }
 
 
 $liczbaPlikow = Get-ChildItem $sciezka -Recurse | Measure-Object -property Length | select-object -ExpandProperty Count #zlicza pliki
 $sumaPlikow = [Math]::Round((Get-ChildItem -Path $sciezka -Recurse | Measure-Object -property Length -Sum).Sum / 1MB, 2)
 
-write-host "Na plycie / w folderze jest:" $liczbaPlikow "pliki / plikow o sumarycznym rozmiarze:" $sumaPlikow "MB"
+Write-Host "Na plycie / w folderze jest:" $liczbaPlikow "pliki / plikow o sumarycznym rozmiarze:" $sumaPlikow "MB"
