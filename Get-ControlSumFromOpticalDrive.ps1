5f9120dfaea92241a63a5aba7bf9238ce9a28c46
Daniel Sajdyk <kontakt@sajdyk.pl>
2020-03-15 16:52:54 +0100

    Regex zmieniono tak, aby obejmowal swoim dzialaniem nie tylko pliki mp3, mp4 itd., ale wszystkie, ktore sa przetwarzane. Przeniesiono takze czesc kodu odpowiadajaca za generowanie podsumowania do sekcji 'if' w ktorej nastepuje generowanie sum kontrolnych.

--- Get-ControlSumFromOpticalDrive.ps1
+++ Get-ControlSumFromOpticalDrive.ps1
@@ -1,6 +1,6 @@
-[CmdletBinding()]
+﻿[CmdletBinding()]
 Param(
-    [Parameter(Mandatory=$false, ValueFromPipeline=$true)]
+    [Parameter(Mandatory=$false, ValueFromPipeline=$false)]
     [ValidateScript({
         if (Test-Path -Path $_){
             $true
@@ -12,25 +12,23 @@ Param(
     $sciezka = (Get-CimInstance Win32_LogicalDisk | Where-Object {$_.DriveType -eq 5} | Select-Object DeviceID -ExpandProperty DeviceID)
 )
 
-
 $script:i=0
 
-# nie zmieniać obslugi bledow na try catch poniewaz wymaga on wygenerowania bledu przerywajacego, ktory nie wystepuje w przypadku Test-Path
+# nie zmieniac obslugi bledow na try catch poniewaz wymaga on wygenerowania bledu przerywajacego, ktory nie wystepuje w przypadku Test-Path
 if (Test-Path -Path "$sciezka"){
-    get-childitem `
+    Get-Childitem `
     -Path $sciezka -Recurse -File -Exclude "player.zip" | `
-    get-filehash -Algorithm MD5 | `
+    Get-Filehash -Algorithm MD5 | `
     Format-Table -Autosize -property `
     @{Name="Lp";Expression={$script:i.Tostring() + ".";$script:i++}}, `
     @{Name="Algorytm"; Expression = {$_.algorithm}}, hash, `
-    @{Name="Plik"; Expression = {$_.Path -replace '(^.*)\\(.*.mp\d$)','$2'}} # | Out-Printer ; 
+    @{Name="Plik"; Expression = {$_.Path -replace '(^.*)\\(.+$)','$2'}} # | Out-Printer ; 
+    
+    # podsumowanie
+    $liczbaPlikow = Get-ChildItem $sciezka -Recurse | Measure-Object -property Length | select-object -ExpandProperty Count
+    $sumaPlikow = [Math]::Round((Get-ChildItem -Path $sciezka -Recurse | Measure-Object -property Length -Sum).Sum / 1MB, 2)
+    Write-Output "W napedzie / sciezce - $sciezka - jest: $liczbaPlikow pliki o rozmiarze: $sumaPlikow MB"
 }
 else {
-    Write-Error "W NAPEDZIE BRAKUJE PLYTY, BADZ WSKAZANY FOLDER - $sciezka - NIE JEST PRAWIDLOWY."
+    Write-Error -Message "W NAPEDZIE BRAKUJE PLYTY, BADZ WSKAZANY FOLDER - $sciezka - NIE JEST PRAWIDLOWY." -Category InvalidArgument
 }
-
-
-$liczbaPlikow = Get-ChildItem $sciezka -Recurse | Measure-Object -property Length | select-object -ExpandProperty Count #zlicza pliki
-$sumaPlikow = [Math]::Round((Get-ChildItem -Path $sciezka -Recurse | Measure-Object -property Length -Sum).Sum / 1MB, 2)
-
-Write-Host "Na plycie / w folderze jest:" $liczbaPlikow "pliki / plikow o sumarycznym rozmiarze:" $sumaPlikow "MB"
